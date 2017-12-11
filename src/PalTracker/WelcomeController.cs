tygern/pal-tracker-dotnet

Add manifest file for deployment to PCF

src/PalTracker/Controllers/WelcomeController.cs
@@ -5,10 +5,15 @@ namespace PalTracker.Controllers
     [Route("/")]
     public class WelcomeController : Controller
     {
+        
+        private readonly Message _message;
+        
         [HttpGet]
-        public string SayHello()
+        public string SayHello() => _message.WelcomeMessage;
+
+        public WelcomeController(Message message)
         {
-            return "hello";
+            _message = message;
         }
     }
 }

markikollasch/pal-tracker

src/PalTracker/WelcomeController.cs
@@ -5,10 +5,14 @@ namespace PalTracker
     [Route("/")]
     public class WelcomeController : Controller
     {
+        private readonly WelcomeMessage _message;
+
         [HttpGet]
-        public string SayHello()
+        public string SayHello() => _message.Message;
+
+        public WelcomeController(WelcomeMessage message)
         {
-            return "hello";
+            _message = message;
         }
     }
 }