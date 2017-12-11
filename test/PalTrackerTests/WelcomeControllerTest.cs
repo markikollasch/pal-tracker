tygern/pal-tracker-dotnet

Add tests for externalized configuration lab

test/PalTrackerTests/WelcomeControllerTest.cs
@@ -1,0 +1,19 @@
+﻿using PalTracker;
+using PalTracker.Controllers;
+using Xunit;
+
+namespace PalTrackerTests
+{
+    public class WelcomeControllerTest
+    {
+        [Fact]
+        public void Get()
+        {
+            var message = new Message("hello from test");
+
+            var controller = new WelcomeController(message);
+
+            Assert.Equal("hello from test", controller.SayHello());
+        }
+    }
+}

markikollasch/pal-tracker

test/PalTrackerTests/WelcomeControllerTest.cs
@@ -1,0 +1,18 @@
+﻿using PalTracker;
+using Xunit;
+
+namespace PalTrackerTests
+{
+    public class WelcomeControllerTest
+    {
+        [Fact]
+        public void Get()
+        {
+            var message = new WelcomeMessage("hello from test");
+
+            var controller = new WelcomeController(message);
+
+            Assert.Equal("hello from test", controller.SayHello());
+        }
+    }
+}